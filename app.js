fa9712c22a49820072831030b23811a4cf9dc24e
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-05-07 08:28:49 +0100

    Completed the jokes generator

---
+++ app.js
@@ -0,0 +1,37 @@
+// Getting the submit button
+let submitNumber = document.querySelector('.get-jokes');
+
+// Adding an event listener to the button
+submitNumber.addEventListener('click', getJokes);
+
+function getJokes(e) {
+  // Get the number input
+  const number = document.querySelector('input[type=number]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  let output = '';
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const res = JSON.parse(this.responseText);
+
+      console.log(res);
+
+      if (res.type === 'success') {
+        res.value.forEach(joke => {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output = '<li>Something went wrong!</li>';
+      }
+      document.querySelector('.output').innerHTML = output;
+    }
+  };
+
+  xhr.send();
+
+  e.preventDefault();
+}
